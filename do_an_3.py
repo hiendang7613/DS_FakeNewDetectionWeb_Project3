5ee04f7a90e86e35c69185dcaa69922308228075
anhtoan1103 <you@example.com>
2022-01-08 16:12:32 +0700

    .

--- do_an_3.py
+++ do_an_3.py
@@ -35,16 +35,6 @@ Nguyễn Vinh Quang - 18120229
 
 
 import warnings
-from sklearn import metrics
-import pickle
-import time
-from sklearn.metrics import RocCurveDisplay
-from sklearn.metrics import roc_curve
-from sklearn.metrics import f1_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
 from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.decomposition import TruncatedSVD
 from sklearn.compose import ColumnTransformer
