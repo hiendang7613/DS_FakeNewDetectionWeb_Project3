c78fa1f26881319acf006fe7839b33e5c746e80d
Hiendang7613 <73353492+hiendang7613@users.noreply.github.com>
2022-01-09 17:36:02 +0700

    update

--- streamlit.py
+++ streamlit.py
@@ -1,63 +1,38 @@
-
-# from do_an_3 import dummy
-# from do_an_3 import MergeCol
-# from do_an_3 import PreprocessDomain
-# from do_an_3 import PreprocessText
 import streamlit as st
-import pandas as pd
 import pickle
-
-import warnings
-from sklearn.base import BaseEstimator, TransformerMixin
-# from sklearn.svm import SVC
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.naive_bayes import MultinomialNB
-# from sklearn.pipeline import Pipeline, make_pipeline
-# from sklearn.preprocessing import FunctionTransformer
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# from sklearn.feature_extraction.text import TfidfTransformer
-# from sklearn.feature_extraction.text import CountVectorizer
 import re
-from vncorenlp import VnCoreNLP
 import pandas as pd
+from vncorenlp import VnCoreNLP
+from sklearn.base import BaseEstimator, TransformerMixin
+import warnings
 warnings.filterwarnings('ignore')
 
-annotator = VnCoreNLP("VnCoreNLP-1.1.1.jar",
-                      annotators="wseg", max_heap_size='-Xmx2g')
-
-# load model
-
-
 def dummy(x): return x
-
-
 class PreprocessText(BaseEstimator, TransformerMixin):
     def __init__(self):
-        self.pattern = "[^a-z0-9A-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ ]*"
-        with open("vietnamese-stopwords.txt", encoding="utf-8") as f:
-            self.stop_word = f.read().splitlines()
-        return
+      self.pattern = "[^a-z0-9A-Z_ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ ]*"
+      with open("vietnamese-stopwords.txt",encoding="utf-8") as f :
+        self.stop_word = f.read().splitlines()
+      return
 
     def fit(self, X_df, y=None):
-        return self
-
+      return self
+      
     def transform(self, X_df, y=None):
-        data = []
-        for text in X_df['text']:
-            # Chuẩn hoá cột text
-            # 1.Loại bỏ ký tự đặc biệt
-            text = re.sub(self.pattern, '', text)
-            # 2.Tách từ
-            text = annotator.tokenize(text)
-            # 3.Loại bỏ stop_word
-            text = [word for word in text[0] if word not in self.stop_word]
-            # 4.Chuẩn hoá chữ thường
-            text = [word.lower() for word in text]
+      data = []
+      for text in X_df['text']:
+        # Chuẩn hoá cột text
+        # 1.Loại bỏ ký tự đặc biệt
+        text = re.sub(self.pattern, '',text)
+        # 2.Tách từ
+        text = annotator.tokenize(text)
+        # 3.Loại bỏ stop_word
+        text = [word for word in text[0] if word not in self.stop_word]
+        # 4.Chuẩn hoá chữ thường
+        text = [word.lower() for word in text]
 
-            data.append(text)
-        return pd.DataFrame({'text': data})
-
+        data.append(text)
+      return pd.DataFrame({'text':data})
 
 class PreprocessDomain(BaseEstimator, TransformerMixin):
     def __init__(self):
@@ -82,7 +57,6 @@ class PreprocessDomain(BaseEstimator, TransformerMixin):
             data.append(domain)
         return pd.DataFrame({'domain': data})
 
-
 class MergeCol(BaseEstimator, TransformerMixin):
     def __init__(self):
         return
@@ -97,32 +71,47 @@ class MergeCol(BaseEstimator, TransformerMixin):
         return data
 
 
-RFCModel = pickle.load(open("model/rfc_.model", 'rb'))
-LGRModel = pickle.load(open("model/lgr_.model", 'rb'))
-MNBModel = pickle.load(open("model/mnb_.model", 'rb'))
-SVCModel = pickle.load(open("model/svc_.model", 'rb'))
 
+annotator = VnCoreNLP("VnCoreNLP-1.1.1.jar", annotators="wseg", max_heap_size='-Xmx2g')
+
+
+""" == Load Model == """
+SVCModel = pickle.load(open("model/svc_.model", 'rb'))
+LGRModel = pickle.load(open("model/lgr_.model", 'rb'))
+PACModel = pickle.load(open("model/pac_.model", 'rb'))
+RFCModel = pickle.load(open("model/rfc_.model", 'rb'))
+GBCModel = pickle.load(open("model/gbc_.model", 'rb'))
+MNBModel = pickle.load(open("model/mnb_.model", 'rb'))
 
 def main():
     # define model
-
     st.header("Fake News Detection Web")
+
     st.subheader("1. Nhập đoạn văn bản bạn muốn kiểm tra:")
     content = pd.DataFrame({'text': [''], 'domain': ['']})
     content.text = st.text_input("Nhập vào đoạn văn bản")
+
     st.subheader("2. Chọn mô hình bạn mong muốn:")
     df = pd.DataFrame({
-        'modelName': ["LogisticRegression", "RandomForestClassifier", "MultinomialNB", "SVC"],
-        'value': [1, 2, 3, 4]
+        'modelName': [
+            "Support Vector Classification",
+            "Logistic Regression", 
+            "Passive Aggressive Classifier", 
+            "Random Forest Classifier", 
+            "Gradient Boosting Classifier", 
+            "Multinomial Naive Bayes"],
+        'value': [1, 2, 3, 4, 5, 6]
     })
     rs = []
     model = st.selectbox(
         'Chọn mô hình',
         df['modelName'])
+
     st.subheader("3. Nhập nguồn bài báo")
     content.domain = st.text_input("Nhập nguồn tin, nếu không có thì bỏ trống")
     if(content.domain[0] == ''):
         content.domain = 'a'
+    
     button = st.button("Kiểm tra")
     thongBao = st.empty()
     if button:
@@ -130,21 +119,29 @@ def main():
             if not len(content):
                 thongBao.markdown("In put at least a piece of news")
             else:
-                if(model == 'LogisticRegression'):
-                    rs = LGRModel.predict(content)
-                if(model == 'RandomForestClassifier'):
-                    rs = RFCModel.predict(content)
-                if(model == 'MultinomialNB'):
-                    rs = MNBModel.predict(content)
-                if(model == 'SVC'):
+                if(model == 'Support Vector Classification'):
                     rs = SVCModel.predict(content)
+                if(model == 'Logistic Regression'):
+                    rs = LGRModel.predict(content)
+                if(model == 'Passive Aggressive Classifier'):
+                    rs = PACModel.predict(content)
+                if(model == 'Random Forest Classifier'):
+                    rs = RFCModel.predict(content)
+                if(model == 'Gradient Boosting Classifier'):
+                    rs = GBCModel.predict(content)
+                if(model == 'Multinomial Naive Bayes'):
+                    rs = MNBModel.predict(content)
                 st.subheader("4. Kết quả:")
                 if(rs[0] == 1):
-
                     st.write("Tin giả")
                 else:
                     st.write("Tin thật")
 
-
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
